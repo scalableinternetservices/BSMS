535fd46654323df81eea6168a5709cc41c0f779c
Rey Tang <tang.changrey@gmail.com>
2018-10-14 00:17:59 -0700

    fixed some controller logic

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,48 +1,42 @@
 class PreferencesController < ActionController::Base
 	before_action :authenticate_user!
 
-	def index
-		@user = current_user
-		@preferences = Preferences.new
-  		# @preferences = current_user.preferences.all
-    	# @all_preferences = Preferences.all
-  	end
-
 	def show
-    	@preferences = Preferences.find(params[:id])
-    	@user = User.find(@preference.user_id)
+		@user = current_user
+		@preference = Preferences.where(user_id: @user.id)
 	end
 
 	def new
+		@preference = Preferences.new
+	end
+
+	def edit
 		@user = current_user
-		@preferences = Preferences.new
-  	end
+		@preference = Preferences.where(user_id: @user.id)
+	end
 
-  	def edit
-    	@preferences = Preferences.find(params[:id])
-  	end
+	def update
+		@user = current_user
+		@preference = Preferences.where(user_id: @user.id)
+		@preference.update(preferences_params)
+		redirect_to preferences_path
+	end
 
-  	def update
-  		@preferences = Preferences.find(params[:id])
-  		@preferences.update(preferences_params)
-  		redirect_to preferences_path
-  	end
-
-  	def create
-  		@user = current_user
-    	@preferences = Preferences.new(preferences_params)
-    	@preferences.user_id = @user.id
-    	@preferences.save
-  		redirect_to preferences_path
-	end	
+	def create
+		@user = current_user
+		@preference = Preferences.new(preferences_params)
+		@preference.user_id = current_user.id
+		@preference.save
+		@user.preferences = @preference
+		redirect_to preferences_path
+	end
 
 	private
 
 	def preferences_params
-		params.require(:preferences).permit(
-								:bedrooms,
-	    						:price,
-	    						:weather,
-	    						:parking)
-  	end
+		params.require(:preferences).permit(:bedrooms,
+																				:price,
+																				:weather,
+																				:parking)
+	end
 end
