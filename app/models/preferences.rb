d86ba8d8569174d99d8200daf7995e10a0396e73
John Lau <johnlau@ResNet-40-75.resnet.ucsb.edu>
2018-10-14 15:01:04 -0700

    Changed preferences to drop down, added more preferences

--- app/models/preferences.rb
+++ app/models/preferences.rb
@@ -1,4 +1,9 @@
 class Preferences < ActiveRecord::Base
   belongs_to :user
+
   validates :user_id, presence: true
+  validates :bedrooms, numericality: {only_integer: true}
+  # validates :price, numericality: {only_integer: true}
+  validates :parking, numericality: {only_integer: true}
+
 end
