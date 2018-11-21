b4f1cf602583a0e31391010be1b2067862ef0215
Rey Tang <tang.changrey@gmail.com>
2018-11-20 16:54:32 -0800

    created tsung xml files for load testing and updated rails app to handle

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,34 +11,50 @@ names = ["Jack", "Max", "Allison", "Tommy", "Rebecca", "Alex", "Jessica", "Micha
 password = "seedaccount"
 
 # Listing parameters
-titles = ["Nice place available for rent!", "Beautiful home available", "House for rent in great location"]
-locations = ["Cupertino", "Fremont", "San Francisco", "Santa Barbara", "Los Angeles", "New York", "Palo Alto", "Saratoga", 
+titles = ["Nice place available for rent!", "Beautiful home available", "House for rent in great location", "Amazing location with view",
+					"Quiet and quaint house", "House on the hill: Gorgeous Mountain Views"]
+locations = ["Cupertino", "Fremont", "San Francisco", "Santa Barbara", "Los Angeles", "New York", "Palo Alto", "Saratoga",
 	"Long Beach", "Orange County", "Venice", "San Diego", "Santa Monica", "Berkeley", "Pleasanton", "La Canada", "Thousand Oaks"]
 prices = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
-housing_types = ["house", "apartment", "condo", "townhouse"]
+housing_types = ["Home", "Condo", "Apartment"]
 descriptions = ["Beautiful place in great location!", "Small and cozy place perfect for 1 or 2 people",
  "Extremely spacious and in a beatufiul city!", "Located in a safe and quiet neighborhood"]
 bedrooms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 bathrooms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-parking_spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-start_date = "2015-11-03"
-end_date = "2015-11-15"
+parking = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+start_date = ["2015-11-03", "2015-11-15", "2015-12-30", "2015-10-23"]
+end_date = ["2016-11-03", "2017-11-15", "2016-12-30", "2016-10-23"]
 random_bool = [true, false]
 
-# TODO: Create Listing requests, contracts, reviews. These relations are little trickier to seed
-
-
-for i in 1..100 do
+for i in 1..1000 do
 	email = "email" + i.to_s + "@bsms.com"
 	user = User.create(email: email, name: names.sample, password: password)
 
+	preferences = Preferences.create(
+		bedrooms: bedrooms.sample,
+		bathrooms: bathrooms.sample,
+		gym: random_bool.sample,
+		pet_friendly: random_bool.sample,
+		ac: random_bool.sample,
+		heater: random_bool.sample,
+		wifi: random_bool.sample,
+		wash_and_dry: random_bool.sample,
+		yard: random_bool.sample,
+		public_transportation: random_bool.sample,
+		parking: parking.sample,
+		price: prices.sample
+	)
+
+	user.preferences = preferences
+
 	for j in 1..5 do
-		listing = Listing.create(title: titles.sample, 
-			location: locations.sample, 
-			price: prices.sample, 
-			start_date: start_date, 
-			end_date: end_date, 
-			housing_type: housing_types.sample, 
+		listing = Listing.create(
+			title: titles.sample,
+			location: locations.sample,
+			price: prices.sample,
+			start_date: start_date.sample,
+			end_date: end_date.sample,
+			housing_type: housing_types.sample,
 			bedrooms: bedrooms.sample,
 			bathrooms: bathrooms.sample,
 			gym: random_bool.sample,
@@ -49,9 +65,10 @@ for i in 1..100 do
 			wash_and_dry: random_bool.sample,
 			yard: random_bool.sample,
 			public_transportation: random_bool.sample,
-			description: descriptions.sample, 
-			available: true)
-
+			parking: parking.sample,
+			description: descriptions.sample,
+			available: true
+		)
 		user.listings << listing
 	end
 end
